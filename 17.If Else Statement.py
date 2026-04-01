91f00247e3af03375ad8bdc943994dcf51b69320
Arockianeopolean <42064263+Arockianeopolean@users.noreply.github.com>
2026-04-01 16:41:30 +0530

    Change integer check from even/odd to positive/negative

--- 17.If Else Statement.py
+++
@@ -1,5 +0,0 @@
-a=int(input("Enter the Number:"))
-if(a%2 == 0):
-	print("This is a Even Integer Value:", a)
-else:
-	print("This is a Odd Integer Value:", a)
---
+++ DAY - 06 & DAY - 07/If Else Statement.py
@@ -0,0 +1,5 @@
+a=int(input("Enter the Number:"))
+if(a >= 0):
+	print("This is a +ve Integer Value:", a)
+else:
+	print("This is a -ve Integer Value:", a)
